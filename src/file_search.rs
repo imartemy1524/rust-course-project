b80a565e3028f5a02b9ee7812c1a1052e494170c
im <imartemy1524@gmail.com>
2026-04-26 20:15:37 +0300

    Second task done

--- src/file_search.rs
+++ src/file_search.rs
@@ -1,15 +1,42 @@
+use crate::test;
+use std::path::PathBuf;
 use std::{fs, io};
 
-pub fn file_search(folder: &str) -> Result<(), io::Error> {
+pub struct SearchArgs {
+    pub folder: PathBuf,
+    pub padding: u32,
+    pub find: Option<PathBuf>,
+}
+pub fn file_search(
+    SearchArgs {
+        folder,
+        padding,
+        find,
+    }: SearchArgs,
+) -> Result<(), io::Error> {
     let files = fs::read_dir(folder)?;
-
-
     for file in files {
         let file = file?;
+        match find.as_ref().and_then(|p| p.file_name()) {
+            Some(name) if name == file.file_name() => {
+                println!(
+                    "Found {} at {}",
+                    name.to_string_lossy(),
+                    file.path().display()
+                );
+            }
+            None => println!("{}", file.file_name().to_string_lossy()),
+            _ => {}
+        }
 
-        println!("{}", file.file_name().to_string_lossy());
+        if file.file_type()?.is_dir() {
+            file_search(SearchArgs {
+                folder: file.path(),
+                padding,
+                find: find.clone(),
+            })?;
+        }
     }
-
 
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,26 @@
 mod file_search;
+mod test;
 
+use std::path::PathBuf;
 use argh::FromArgs;
-use crate::file_search::file_search;
+use crate::file_search::{file_search, SearchArgs};
 
 #[derive(FromArgs)]
 ///
 struct Arguments {
     /// whether or not to jump
     #[argh(positional, description = "folder to search")]
-    name: String,
+    folder: PathBuf,
+
+    #[argh(option, description = "find")]
+    find: Option<PathBuf>
 }
 
 fn main() {
     let args: Arguments = argh::from_env();
-    file_search(args.name.as_str()).unwrap();
+    file_search(SearchArgs{
+        padding: 0u32,
+        folder: args.folder,
+        find: args.find,
+    }).unwrap();
 }
